1abff212b07a67fe6cf1700a7577eb85bb0bfe2c
Ann-Annice <annannice98@gmail.com>
2022-10-10 20:00:33 +0530

    todolistcopy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TodolistcopyComponent } from './todolistcopy/todolistcopy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodolistcopyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todolistcopy/todolistcopy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolistcopy',
+  templateUrl: './todolistcopy.component.html',
+  styleUrls: ['./todolistcopy.component.css']
+})
+export class TodolistcopyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
